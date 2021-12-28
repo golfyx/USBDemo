5855b56ba990d236ed01b0bbb1dc3f0d0357c41b
Golfy.Xiong@semacare.com <golfy.xiong@semacare.com>
2021-12-28 10:43:02 +0800

    添加nsarray分类，将log打印显示成中文

---
+++ USBDemo/Handles/LogHandleManageModule/NSArray+Log.h
@@ -0,0 +1,20 @@
+//
+//  NSArray+Log.h
+//  USBDemo
+//
+//  Created by golfy on 2021/12/27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSArray (Log)
+
+@end
+
+@interface NSDictionary (Log)
+
+@end
+
+NS_ASSUME_NONNULL_END
